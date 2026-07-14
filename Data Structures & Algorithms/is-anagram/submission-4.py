aa21ca17f008e7fbc05ff24050e9e671c2d1ea1e
ahmedbarakatt1 <123165403+ahmedbarakatt1@users.noreply.github.com>
2026-07-14 03:11:26 +0300

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        chars= []
+        if len(s) != len(t):
+            return False
+        else:
+            s = sorted(s)
+            t = sorted(t)
+            for i in range(len(s)):
+                if s[i] != t[i]:
+                    return False
+            return True
+
+        
+            
+        
